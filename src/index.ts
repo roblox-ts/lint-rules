9b15ecfa4a0ac0ef9c100a2b23d5902d1e2992c6
MobiDev <developmentmobius@gmail.com>
2021-06-11 09:37:27 +0000

    add noPrecedingSpreadElement

--- src/index.ts
+++ src/index.ts
@@ -68,6 +68,7 @@ export = makePlugin({
 					"no-value-typeof": "error",
 					"no-private-identifier": "error",
 					"no-spread-destructuring": "error",
+					"no-preceding-spread-element": "error",
 					module: "error",
 
 					"misleading-luatuple-checks": "warn",
